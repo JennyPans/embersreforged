ab3660153c945e1a204c25b5b11b96e90379b68f
JennyPans <jennypans@outlook.com>
2021-12-04 18:10:09 +0100

    Add an infused shard

---
+++ src/main/java/embersreforged/EmbersReforged.java
@@ -0,0 +1,21 @@
+package main.java.embersreforged;
+
+import main.java.embersreforged.common.item.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(EmbersReforged.MODID)
+public class EmbersReforged {
+	
+	public static final String MODID = "embersreforged";
+	
+	public EmbersReforged() {
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		Items.register(eventBus);
+		
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
---
+++ src/main/java/embersreforged/common/item/Items.java
@@ -0,0 +1,20 @@
+package main.java.embersreforged.common.item;
+
+import main.java.embersreforged.EmbersReforged;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Items {
+	
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EmbersReforged.MODID);
+	public static final RegistryObject<Item> INFUSED_SHARD = ITEMS.register("infused_shard", 
+			() -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+	
+	public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
+}
